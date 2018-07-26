875b76686c36f9a8ffdb33cf5d3456181df38a06
hejianjun@guoshengtianfeng.com <hejianjun@guoshengtianfeng.com>
2018-07-26 14:15:18 +0800

    add git

--- app/src/main/java/com/example/helang/volumewave/VolumeWaveView.java
+++ app/src/main/java/com/example/helang/volumewave/VolumeWaveView.java
@@ -13,24 +13,25 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.view.animation.DecelerateInterpolator;
 
+import java.util.Random;
+
 
 /**
- * 波浪...
+ * 仿百度的语音助手--波浪动画控件
  */
 public class VolumeWaveView extends View {
     private static final String TAG = "VolumeWaveView";
 
-    private static final int HEIGHT = 300;//整个曲线的高度
-    private static final int HEIGHT1 = 100;//第一层曲线的高度
-    private static final int HEIGHT2 = 300;//第二层曲线的高度
-    private static final int HEIGHT3 = 250;//第三层曲线的高度
 
-    private int leftRightMargin = 50;
-    
-    private int parentWith = 0;
+    private static final int HEIGHT = 400;//整个控件的高度
 
+    private static final int HEIGHT1 = 200;//第一层曲线的高度
+    private static final int HEIGHT2 = 400;//第二层曲线的高度
+    private static final int HEIGHT3 = 350;//第三层曲线的高度
 
     private float h1 = 200,h2 = 200, h3 = 300,h4 = 300,h5 = 200;
+
+    private int range = 0;//波动的幅度,你可以动态改变这个值，比如麦克风录入的音量的高低
 
     private static final int splitWidth = -200;//扇形的交错距离
 
@@ -38,6 +39,8 @@ public class VolumeWaveView extends View {
     private Paint paint1,paint2,paint3,paint4;
 
     private LinearGradient linearGradient1,linearGradient2,linearGradient3,linearGradient4;//四种渐变色
+
+    private ValueAnimator animator1,animator2,animator3,animator4,animator5;//五种动画
 
 
     public VolumeWaveView(Context context) {
@@ -52,7 +55,7 @@ public class VolumeWaveView extends View {
         super(context, attrs,defStyleAttr);
         initPaint();
 
-        
+
     }
 
     private void initPaint(){
@@ -60,12 +63,14 @@ public class VolumeWaveView extends View {
 
         paint1 = new Paint();
         paint1.setStyle(Paint.Style.FILL);
-        //渐变色1  9052a6d2  9052d5a1
+        paint1.setAntiAlias(true);//抗锯齿
+        //渐变色1
         linearGradient1 = new LinearGradient(0, 0, 0, HEIGHT1,
                 Color.parseColor("#e652a6d2"), Color.parseColor("#e652d5a1"), Shader.TileMode.MIRROR);
         paint1.setShader(linearGradient1);
 
         paint2 = new Paint();
+        paint2.setAntiAlias(true);//抗锯齿
         paint2.setStyle(Paint.Style.FILL);
         //渐变色2
         linearGradient2 = new LinearGradient(0, 0, 0, HEIGHT2,
@@ -74,6 +79,7 @@ public class VolumeWaveView extends View {
 
 
         paint3 = new Paint();
+        paint3.setAntiAlias(true);//抗锯齿
         paint3.setStyle(Paint.Style.FILL);
         //渐变色3
         linearGradient3 = new LinearGradient(0, 0, 0, HEIGHT3,
@@ -82,6 +88,7 @@ public class VolumeWaveView extends View {
 
 
         paint4 = new Paint();
+        paint4.setAntiAlias(true);//抗锯齿
         paint4.setStyle(Paint.Style.FILL);
         //渐变色4
         linearGradient4 = new LinearGradient(0, 0, 0, HEIGHT2,
@@ -93,6 +100,10 @@ public class VolumeWaveView extends View {
     }
 
 
+    /**
+     * draw方法中不要创建大量对象，尽量复用对象
+     * @param canvas
+     */
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
@@ -148,7 +159,10 @@ public class VolumeWaveView extends View {
      * 添加属性动画
      */
     public void startAnimation() {
-        ValueAnimator animator1 = ValueAnimator.ofInt(0,HEIGHT1,0);
+        Random random = new Random();
+        range = random.nextInt(100)%(100-10+1) + 10;//波动的幅度，模拟动态音量输入，你可以自己设置
+
+        animator1 = ValueAnimator.ofInt(0,HEIGHT1,0);
         animator1.setDuration(1400);
         animator1.setInterpolator(new DecelerateInterpolator());
         //无限循环
@@ -164,7 +178,7 @@ public class VolumeWaveView extends View {
         });
         animator1.start();
 
-        ValueAnimator animator2 = ValueAnimator.ofInt(0,HEIGHT1,0);
+        animator2 = ValueAnimator.ofInt(0,HEIGHT1,0);
         animator2.setDuration(1700);
         animator2.setInterpolator(new DecelerateInterpolator());
         //无限循环
@@ -182,8 +196,7 @@ public class VolumeWaveView extends View {
 
 
 
-
-        ValueAnimator animator3 = ValueAnimator.ofInt(0,HEIGHT2,0);
+        animator3 = ValueAnimator.ofInt(0,HEIGHT2,0);
         animator3.setDuration(1600);
         animator3.setInterpolator(new DecelerateInterpolator());
         //无限循环
@@ -192,7 +205,7 @@ public class VolumeWaveView extends View {
 
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
-                h3 = (int) animation.getAnimatedValue();
+                h3 = (int) animation.getAnimatedValue() + range;
                 invalidate();
 
             }
@@ -200,7 +213,7 @@ public class VolumeWaveView extends View {
         animator3.start();
 
 
-        ValueAnimator animator4 = ValueAnimator.ofInt(0,HEIGHT2,0);
+        animator4 = ValueAnimator.ofInt(0,HEIGHT2,0);
         animator4.setDuration(1300);
         animator4.setInterpolator(new DecelerateInterpolator());
         //无限循环
@@ -217,7 +230,7 @@ public class VolumeWaveView extends View {
         animator4.start();
 
 
-        ValueAnimator animator5 = ValueAnimator.ofInt(0,HEIGHT2,0);
+        animator5 = ValueAnimator.ofInt(0,HEIGHT2,0);
         animator5.setDuration(1250);
         animator5.setInterpolator(new DecelerateInterpolator());
         //无限循环
@@ -232,6 +245,33 @@ public class VolumeWaveView extends View {
             }
         });
         animator5.start();
+    }
+
+
+    /**
+     * 关闭动画
+     */
+    public void removeAnimation(){
+        if (animator1 != null){
+            animator1.cancel();
+            animator1 = null;
+        }
+        if (animator2 != null){
+            animator2.cancel();
+            animator2 = null;
+        }
+        if (animator3 != null){
+            animator3.cancel();
+            animator3 = null;
+        }
+        if (animator4 != null){
+            animator4.cancel();
+            animator4 = null;
+        }
+        if (animator5 != null){
+            animator5.cancel();
+            animator5 = null;
+        }
     }
 
 
